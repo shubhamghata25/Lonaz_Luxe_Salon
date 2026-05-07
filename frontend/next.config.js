371565cb89d62216d13574cf1339ac19651e88d5
Shubhamghata <shubhamghata25@gmail.com>
2026-05-07 20:34:54 +0530

    your message

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,6 +10,15 @@ const nextConfig = {
   },
   poweredByHeader: false,
   compress: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/backend/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
